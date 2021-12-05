4d07cc2ab7b82c39b414f90e24c02c71b240f1ce
mayara bueno <luziamayara83@gmail.com>
2021-12-05 12:37:09 -0300

    finalização jj

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,7 +19,7 @@ if(navClose){
     })
 }
 
-/*==================== REMOVE MENU MOBILE ====================*/
+/*==================== REMOVE MENU o MOBILE ====================*/
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
